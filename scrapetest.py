f96d3857a1f1056ae66f9d96048dfa5bb6f1cc85
dwrank <iMac@Apples-iMac.local>
2015-11-20 00:51:06 -0800

    Update scrapetest to use BeautifulSoup

--- scrapetest.py
+++ scrapetest.py
@@ -1,7 +1,47 @@
 #!/usr/bin/env python
 
-from urllib.request import urlopen
+import requests
+from pprint import pprint
+from bs4 import BeautifulSoup
 
 
-html = urlopen("http://pythonscraping.com/pages/page1.html")
-print(html.read())
+def request_url(url):
+    url_resp = None
+    
+    try:
+        resp = requests.get(url)
+    except requests.exceptions.ConnectionError as e:
+        print('Connection Error')
+    except requests.exceptions.ConnectTimeout as e:
+        print('Connection Timeout')
+    except requests.exceptions.ReadTimeout as e:
+        print('Read Timeout')
+    except requests.exceptions.HTTPError as e:
+        print('HTTP Error: %s' % e.message)
+    else:
+        if resp.status_code != requests.codes.ok:
+            print('HTTP Error: %d' % resp.status_code)
+        else:
+            url_resp = resp
+    
+    return url_resp
+
+
+def get_title(url):
+    resp = request_url(url)
+    if resp == None:
+        return None
+    
+    if resp.headers["content-type"] != "text/html":
+        print('HTTP Error, Unexpected Header Content Type: %s'
+              % resp.headers["content-type"])
+        return None
+    else:
+        soup = BeautifulSoup(resp.text, 'html5lib')
+        pprint(soup.h1)
+
+
+if __name__ == '__main__':
+    url = "http://pythonscraping.com/pages/page1.html"
+
+    get_title(url)
