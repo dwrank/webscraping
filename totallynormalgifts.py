72dc78bf31454209ee63af6799c69a2b0e7cd4b5
drank <drank@drank-XPS-8500.(none)>
2015-11-22 17:54:59 -0800

    Add totallynormalgifts.py

---
+++ totallynormalgifts.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from pprint import pprint
+from bs4 import BeautifulSoup
+
+import webscrapingutil as util
+
+
+def get_title(url):
+    resp = util.request_url(url)
+    if resp == None:
+        return None
+    
+    if resp.headers["content-type"] != "text/html":
+        print('HTTP Error, Unexpected Header Content Type: %s'
+              % resp.headers["content-type"])
+        return None
+    else:
+        soup = BeautifulSoup(resp.text, 'html5lib')
+        pprint(soup.h1)
+
+
+if __name__ == '__main__':
+    url = "http://www.pythonscraping.com/pages/page3.html"
+    soup = util.create_soup(url)
+    
+    gifts = soup.findAll(class_='gift')
+    
+    gift_list = []
+    
+    for gift in gifts:
+        tds = [r for r in gift.findAll('td')]
+        d = {}
+        d['title'] = tds[0].text.strip()
+        d['description'] = tds[1].text.strip()
+        d['cost'] = tds[2].text.strip()
+        d['image'] = tds[3].img['src']
+        gift_list.append(d)
+    
+    pprint(gift_list)
